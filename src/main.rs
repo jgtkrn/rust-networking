af1994dd884b53e31c862ee048fd6fb159f2c543
jgtkrn <jagatkirana.jk@gmail.com>
2024-05-06 13:03:15 +0800

    NET-0002: simple send receive

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+#[allow(unused_imports)]
+use std::{error::Error, net::{TcpListener, TcpStream}};
 
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0;1024];
-    stream.read(&mut buffer).expect("failed to read message");
-    let request = String::from_utf8_lossy(&buffer[..]);
-    println!("request received: {}", &request);
-    let response = "Hello client".as_bytes();
-    stream.write(response).expect("failed write response");
+fn handle_client(mut stream: TcpStream){
+    loop {
+        let mut buffer = [0;1024];
+        // let response = b"+PONG\r\n";
+        match stream.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(_) => {
+                stream.write(buffer.as_slice());
+            },
+            Err(e) => {
+                eprintln!("failed to read stream, {e}");
+                break;
+            }
+        }
+    }
 }
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3000")?;
@@ -16,9 +25,7 @@ fn main() -> std::io::Result<()> {
     // accept connections and process them serially
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                std::thread::spawn(|| handle_client(stream));
-            },
+            Ok(stream) => handle_client(stream),
             Err(e) => eprintln!("failed to handle client, error: {}", e)
         }
     }
